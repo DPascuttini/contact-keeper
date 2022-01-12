c366ef60ff1177fb6a2239503f0d9e2b6dc6a501
Dominik Pascuttini <dominik.pascuttini1@gmail.com>
2022-01-12 16:45:41 +0100

    log in auth but something doesnt work right

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,21 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const express = require("express");
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const User = require("../models/User");
 
 //@route GET api/auth
 //@desc Get logged in user
 //@access Private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+  } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+  }
 });
 
 //@route POST api/auth
@@ -35,13 +42,13 @@ router.get(
       let user = await User.findOne({ email });
 
       if (!user) {
-        return res.status(400).json({ msg: "Invalid Credentials" });
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({ msg: "Invalid Credentials" });
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
 
       const payload = {
